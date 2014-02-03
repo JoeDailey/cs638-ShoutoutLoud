0b6399843aa62ef6a20d7e704c8f8106a00a1103
JoeDailey <JosephLDailey@gmail.com>
2014-02-02 23:42:52 -0600

    pulling

--- ShoutOutLoud/public/javascripts/main.js
+++ ShoutOutLoud/public/javascripts/main.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 				type:"GET",
 				dataType:"json",
 				success:function(user,status){
-					$(".content").append($('<div class="popup-profile"><div class="popup-boxer col-md-3"></div><div class="col-md-6"><div class="shout-container"><div class="shout"><div id="them"><img src="http://placehold.it/100x100"><h1>'+user.fullName+'</h1><h2>@'+user.handle+'</h2></div><div class="profile-stats-container"><div class="profile-stats"><div><p>Tweets</p><h3>'+user.numTweets+'</h3></div><div><p>Followers</p><h3>'+user.numFollowers+'</h3></div><div><p>Following</p><h3>'+user.numFollowing+'</h3></div></div></div></div></div></div><div class="popup-boxer col-md-3"></div></div>'));
+					$(".content").append($('<div class="popup-profile"><div class="popup-boxer col-md-3"></div><div class="col-md-6"><div class="shout-container"><div class="shout"><div id="them"><img src="http://placehold.it/100x100"><h1>'+user.fullName+'</h1><h2>@'+user.handle+'</h2></div><div class="profile-stats-container"><div class="profile-stats"><div class="col-md-4 col-sm-12"><p>Tweets</p><h3>'+user.numTweets+'</h3></div><div class="col-md-4 col-sm-12"><p>Followers</p><h3>'+user.numFollowers+'</h3></div><div class="col-md-4 col-sm-12"><p>Following</p><h3>'+user.numFollowing+'</h3></div><div class="clearfix"></div></div></div></div></div></div><div class="popup-boxer col-md-3"></div></div>'));
 					$('.popup-boxer ').click(function(){
 						console.log('click');
 						$($(this).parent()).remove();
@@ -20,6 +20,9 @@ $(document).ready(function(){
 				}
 			});
 		}
+	});
+	$('.compose').keyup(function(e){
+		console.log(e);
 	});
 });
 
