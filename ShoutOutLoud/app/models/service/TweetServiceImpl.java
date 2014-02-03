708229a9933dbd8e99b72d01c64090e2ffe03a0c
Shishir Kumar Prasad <skprasad@cs.wisc.edu>
2014-02-02 18:31:20 -0800

    Fixed the logic of feed to include tweets from user as well as subscribers.

--- ShoutOutLoud/app/models/service/TweetServiceImpl.java
+++ ShoutOutLoud/app/models/service/TweetServiceImpl.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -65,6 +66,11 @@ public class TweetServiceImpl implements TweetService {
 		if(maxTid == null) {
 			maxTid = Long.MAX_VALUE;
 		}
+		
+		/**
+		 * The set of tweets to show in the feed consist of the tweets made by the user and
+		 * all the users to which this user is subscribed/following.
+		 */
 		List<Tweet> tweets = Lists.newArrayList();
 		List<Tweet> selfTweets = searchService.searchTweetsByHandle(handle, maxTid);
 		tweets.addAll(selfTweets);
@@ -73,9 +79,10 @@ public class TweetServiceImpl implements TweetService {
 
 		long uid = searchService.searchUidForTwitterHandle(handle);
 		
-		String tweetFetchSQL = "SELECT t.id AS tid, t.content, t.create_time FROM " + 
-				Constants.USER_TO_TWEET_TBL + " u JOIN " + Constants.TWEET_TBL + 
-				" t ON(u.tid = t.id) WHERE u.uid = ? AND t.id < ? LIMIT ?";
+		List<Tweet> subscriberTweets = Lists.newArrayList();
+		String tweetFetchSQL = " SELECT f.src_uid AS uid, t.id AS tid, t.content, t.create_time " +
+			" FROM following f JOIN user_to_tweet u ON (f.src_uid = u.uid) " +
+			" JOIN tweet t ON (u.tid = t.id) WHERE f.tgt_uid = ? AND t.id < ? LIMIT ? ";
 		PreparedStatement preparedSql = null;
 		try {
 			preparedSql = dbConn.prepareStatement(tweetFetchSQL);
@@ -88,19 +95,31 @@ public class TweetServiceImpl implements TweetService {
 				long tweetId = results.getLong("tid");
 				String content = results.getString("content");
 				Date timestamp = results.getDate("create_time");
+				long subUid = results.getLong("uid");
 				
 				Tweet tweet = new Tweet(tweetId, content, timestamp);
-				Profile profile = searchService.searchProfileByHandle(handle);
+				Profile profile = searchService.searchProfileByUid(subUid);
 				tweet.setTweetCreator(profile);
-				tweets.add(tweet);
+				subscriberTweets.add(tweet);
 			}
 		} catch (SQLException e) {
 			Logger.error("Failed to load tweet for handle : " + handle);
 			e.printStackTrace();
-		}		
+		}
+		
+		tweets.addAll(subscriberTweets);
+		
+		Collections.sort(tweets);
+		Collections.reverse(tweets);
 		
 		DBUtils.cleanDBResources(dbConn, preparedSql);
-		return tweets;	
+		if(tweets.size() <= Constants.NUM_ENTRIES_PER_PAGE) {
+			return tweets;
+		}
+		else{
+			return tweets.subList(0, Constants.NUM_ENTRIES_PER_PAGE);			
+		}
+
 	}
 
 }
